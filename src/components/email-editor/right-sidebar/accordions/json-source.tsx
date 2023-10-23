167dad340bf3720251518f2a73d13896fa262fcc
knowankit <44323532+knowankit@users.noreply.github.com>
2023-10-23 14:35:51 +0530

    feat: add mjml html

--- src/components/email-editor/right-sidebar/accordions/json-source.tsx
+++ src/components/email-editor/right-sidebar/accordions/json-source.tsx
@@ -9,11 +9,15 @@ import { TextareaAutosize } from "@mui/base";
 import useEmailStore from "@/store/email";
 import { Button } from "@mui/material";
 import { useState } from "react";
+import { SourceAccordionType } from "@/types/email-editor.types";
+interface ILayout {
+  expanded: SourceAccordionType;
+  handleChange: (value: SourceAccordionType) => void;
+}
 
-const JsonSource = () => {
+const JsonSource = ({ expanded, handleChange }: ILayout) => {
   const { emailData, setEmailData } = useEmailStore();
   const [jsonContent, setJSONContent] = useState<any>(emailData);
-  const [expanded, setExpanded] = useState("json");
 
   const applyChanges = () => {
     try {
@@ -27,7 +31,7 @@ const JsonSource = () => {
   return (
     <Accordion
       expanded={expanded === "json"}
-      onChange={() => setExpanded(expanded === "json" ? "" : "json")}
+      onChange={() => handleChange(expanded === "json" ? "" : "json")}
       sx={{ width: "100%" }}
     >
       <AccordionSummary aria-controls="content">
--- src/components/email-editor/right-sidebar/accordions/mjml-source.tsx
+++ src/components/email-editor/right-sidebar/accordions/mjml-source.tsx
@@ -3,24 +3,69 @@ import {
   AccordionDetails,
   AccordionSummary
 } from "@/lib/ui/accordion";
-import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import { useState } from "react";
+import Box from "@mui/material/Box";
 
-const MjmlSource = () => {
-  const [expanded, setExpanded] = useState("mjml");
+import { useState, useEffect } from "react";
+import useEmailStore from "@/store/email";
+import { TextareaAutosize } from "@mui/base";
+
+import { SourceAccordionType } from "@/types/email-editor.types";
+
+interface ILayout {
+  expanded: SourceAccordionType;
+  handleChange: (value: SourceAccordionType) => void;
+}
+const MjmlSource = ({ expanded, handleChange }: ILayout) => {
+  const [htmlData, setHtmlData] = useState("");
+  const { emailData } = useEmailStore();
+
+  useEffect(() => {
+    const loadMjMl = async () => {
+      const url = "http://localhost:3000/api/email-editor/generate-mjml";
+
+      const withHtml = {
+        tagName: "mjml",
+        attributes: {},
+        children: [emailData]
+      };
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(withHtml) // Convert the data to JSON format
+      });
+      const data = await response.json();
+      setHtmlData(data.html);
+    };
+
+    loadMjMl();
+  }, []);
 
   return (
     <Accordion
       expanded={expanded === "mjml"}
-      onChange={() => setExpanded(expanded === "mjml" ? "" : "mjml")}
+      onChange={() => handleChange(expanded === "mjml" ? "" : "mjml")}
       sx={{ width: "100%" }}
     >
       <AccordionSummary aria-controls="content">
         <Typography fontSize="0.8rem">Mjml Source</Typography>
       </AccordionSummary>
       <AccordionDetails>
-        <Box display="flex">Layout</Box>
+        <Box display="flex" sx={{ height: "400px" }}>
+          <TextareaAutosize
+            maxRows={4}
+            disabled
+            style={{
+              width: "100%",
+              overflow: "scroll",
+              height: "inherit",
+              fontFamily: "Signika Negative"
+            }}
+            defaultValue={htmlData}
+          />
+        </Box>
       </AccordionDetails>
     </Accordion>
   );
