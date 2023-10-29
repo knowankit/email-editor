64a167e9401edd878e5ee465a7a080bea8ea037d
knowankit <44323532+knowankit@users.noreply.github.com>
2023-10-29 16:31:23 +0530

    feat: reset template name on close

--- src/components/email-editor/drop-container/create-template-button.tsx
+++ src/components/email-editor/drop-container/create-template-button.tsx
@@ -16,6 +16,7 @@ const CreateTemplateButton = () => {
 
   const handleClick = () => {
     setOpen(prev => !prev);
+    setTemplateName("");
   };
 
   const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
