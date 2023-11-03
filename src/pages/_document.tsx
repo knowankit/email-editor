2e61f4303a2df1f27f327fcc3cfd3c304a88b678
knowankit <44323532+knowankit@users.noreply.github.com>
2023-11-03 08:04:58 +0530

    feat: add site icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,800&family=Signika+Negative:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" href="/assets/image/email-editor-icon.png" />
       </Head>
       <body>
         <Main />
