a315845554845502b106c7dc02c5f9cf3fa9bb97
knowankit <44323532+knowankit@users.noreply.github.com>
2023-11-03 22:09:51 +0530

    feat: add tooltips

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,37 +1,37 @@
-const installEvent = () => {
-  self.addEventListener("install", () => {
-    console.log("service worker installed");
-  });
-};
-installEvent();
+// const installEvent = () => {
+//   self.addEventListener("install", () => {
+//     console.log("service worker installed");
+//   });
+// };
+// installEvent();
 
-const activateEvent = () => {
-  self.addEventListener("activate", () => {
-    console.log("service worker activated");
-  });
-};
+// const activateEvent = () => {
+//   self.addEventListener("activate", () => {
+//     console.log("service worker activated");
+//   });
+// };
 
-activateEvent();
+// activateEvent();
 
-const cacheName = "v1";
+// const cacheName = "v1";
 
-const cacheClone = async e => {
-  const res = await fetch(e.request);
-  const resClone = res.clone();
+// const cacheClone = async e => {
+//   const res = await fetch(e.request);
+//   const resClone = res.clone();
 
-  const cache = await caches.open(cacheName);
-  await cache.put(e.request, resClone);
-  return res;
-};
+//   const cache = await caches.open(cacheName);
+//   await cache.put(e.request, resClone);
+//   return res;
+// };
 
-const fetchEvent = () => {
-  self.addEventListener("fetch", e => {
-    e.respondWith(
-      cacheClone(e)
-        .catch(() => caches.match(e.request))
-        .then(res => res)
-    );
-  });
-};
+// const fetchEvent = () => {
+//   self.addEventListener("fetch", e => {
+//     e.respondWith(
+//       cacheClone(e)
+//         .catch(() => caches.match(e.request))
+//         .then(res => res)
+//     );
+//   });
+// };
 
-fetchEvent();
+// fetchEvent();
--- src/components/drop-container/responsive-control.tsx
+++ src/components/drop-container/responsive-control.tsx
@@ -9,7 +9,7 @@ import { Box } from "@mui/material";
 import useEmailStore from "@/store/email";
 import CreateTemplateButton from "@/components/drop-container/create-template-button";
 import useEmailHistoryStore from "@/store/email-history";
-import Tooltip from '@mui/material/Tooltip';
+import Tooltip from "@mui/material/Tooltip";
 
 interface IResponsiveControl {
   setCurrentView: (view: currentView) => void;
@@ -60,19 +60,21 @@ const ResponsiveControl = ({
         }}
       >
         <Tooltip title="Edit view" placement="top" arrow>
-          <IconButton
-            aria-label="edit view"
-            color={currentView == "edit" ? "primary" : "default"}
-            onClick={() => {
-              setCurrentView("edit");
-              setIsMobile(false);
-            }}
-          >
-            <ComputerIcon />
-          </IconButton>
+          <Box component="span">
+            <IconButton
+              aria-label="edit view"
+              color={currentView == "edit" ? "primary" : "default"}
+              onClick={() => {
+                setCurrentView("edit");
+                setIsMobile(false);
+              }}
+            >
+              <ComputerIcon />
+            </IconButton>
+          </Box>
         </Tooltip>
         <Tooltip title="Mobile view" placement="top" arrow>
-          <Box component='span'>
+          <Box component="span">
             <IconButton
               aria-label="mobile view"
               disabled={!emailData["children"].length}
@@ -85,10 +87,9 @@ const ResponsiveControl = ({
               <SmartphoneIcon />
             </IconButton>
           </Box>
-
         </Tooltip>
         <Tooltip title="Desktop view" placement="top" arrow>
-          <Box component='span'>
+          <Box component="span">
             <IconButton
               aria-label="Desktop view"
               disabled={!emailData["children"].length}
@@ -101,7 +102,6 @@ const ResponsiveControl = ({
               <PreviewIcon />
             </IconButton>
           </Box>
-
         </Tooltip>
       </Box>
       <Box
@@ -110,7 +110,7 @@ const ResponsiveControl = ({
       >
         <Box>
           <Tooltip title="Undo" placement="top" arrow>
-            <Box component='span'>
+            <Box component="span">
               <IconButton
                 aria-label="Undo"
                 disabled={!undoStack.length}
@@ -118,15 +118,14 @@ const ResponsiveControl = ({
               >
                 <UndoIcon />
               </IconButton>
-              </Box>
+            </Box>
           </Tooltip>
           <Tooltip title="Redo" placement="top" arrow>
-            <Box component='span'>
-
+            <Box component="span">
               <IconButton
                 aria-label="Redo"
                 disabled={!redoStack.length}
-                Box={redoEmail}
+                onClick={redoEmail}
               >
                 <RedoIcon />
               </IconButton>
@@ -135,7 +134,7 @@ const ResponsiveControl = ({
         </Box>
         <Box>
           <Tooltip title="Reset" placement="top" arrow>
-            <Box component='span'>
+            <Box component="span">
               <IconButton
                 aria-label="reset"
                 disabled={!emailData["children"].length}
