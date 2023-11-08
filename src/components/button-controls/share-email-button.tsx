cd323fb33efddcbfd43daa9d62534d67892bd212
knowankit <44323532+knowankit@users.noreply.github.com>
2023-11-08 19:15:28 +0530

    hotfix: test 2

--- src/components/button-controls/share-email-button.tsx
+++ src/components/button-controls/share-email-button.tsx
@@ -3,7 +3,7 @@ import SendIcon from "@mui/icons-material/Send";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-// import useEmailDataStore from "@/store/email";
+import useEmailDataStore from "@/store/email";
 
 const ShareModal = dynamic(() => import("@/components/share-modal"), {
   ssr: false
@@ -12,7 +12,7 @@ const ShareModal = dynamic(() => import("@/components/share-modal"), {
 const ShareEmailButton = () => {
   const [isOpen, setModalVisibility] = useState(false);
   const { status } = useSession();
-  // const { emailData } = useEmailDataStore();
+  const { emailData } = useEmailDataStore();
 
   return (
     <>
@@ -20,7 +20,9 @@ const ShareEmailButton = () => {
         color="secondary"
         className="step-7"
         size="small"
-        disabled={!(status === "authenticated")}
+        disabled={
+          !(status === "authenticated") || !emailData["children"].length
+        }
         onClick={() => setModalVisibility(true)}
         sx={{ textTransform: "none", mr: "1rem" }}
       >
