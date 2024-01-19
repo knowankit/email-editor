4c051e0e9e15ea687336f86bf3740d8c23b858d1
knowankit <44323532+knowankit@users.noreply.github.com>
2024-01-19 13:19:21 +0530

    feat: add next script

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import EditorSkeletonLoader from "@/components/editor-skeleton-loader";
+import Script from "next/script";
 
 const EmailEditor = dynamic(() => import("@/components"), {
   ssr: false,
@@ -18,22 +19,6 @@ const EmailEditorPage = () => {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ANALYTICS}`}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GA_ANALYTICS}', {
-                page_path: window.location.pathname
-              });
-            `
-          }}
-        />
       </Head>
       <EmailEditor />
     </Box>
